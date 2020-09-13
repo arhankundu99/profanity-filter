c398dbc5cdbf546d4b11dbbde613d132bce5b934
Arhan Kundu <arhankundu99@gmail.com>
2020-09-13 15:41:07 +0530

    made it faster!
    removed unnecessary dfs and added more features like blocking profane urls, adding profane words and whitelist words

--- profanity.py
+++ profanity.py
@@ -5,24 +5,61 @@ from io import BytesIO
 from trie import Trie
 
 
+def get_image_profanity_score(image_url):
+    r = requests.post(
+        "https://api.deepai.org/api/nsfw-detector",
+        data={
+            'image': image_url,
+        },
+        headers={'api-key': '7b0ebb62-4127-46a7-877f-2d520f635a75'}
+    )
+    # if nsfw_score is more than 0.7 it is definitely profane
+    return r.json()['output']['nsfw-score']
+
+
+def get_image_analysis(url):
+    r = requests.post(
+        "https://api.deepai.org/api/nsfw-detector",
+        data={
+            'image': url,
+        },
+        headers={'api-key': '7b0ebb62-4127-46a7-877f-2d520f635a75'}
+    )
+    return r.json()['output']
+
+
+def censor_image(image_url):
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+
+    # Applying GaussianBlur filter
+    gaussImage = img.filter(ImageFilter.GaussianBlur(100))
+    # gaussImage.show()
+
+    # Save Gaussian Blur Image
+    image_name = hash(image_url)
+    gaussImage.save('images/'+str(image_name)+'.jpg')
+
+
 class ProfanityFilter:
     def __init__(self):
-        self.CHARS_MAPPING = {
-            "a": ("a", "@", "*", "4"),
-            "b": ("b", "6"),
-            "i": ("i", "*", "l", "1"),
-            "o": ("o", "*", "0", "@"),
-            "u": ("u", "*", "v"),
-            "v": ("v", "*", "u"),
-            "l": ("l", "1", "I"),
-            "e": ("e", "*", "3"),
-            "s": ("s", "$", "5"),
-            "t": ("t", "7")
-        }
+        self.censor_urls = set()
         self.profane_trie = Trie()
         self.whiteList_trie = Trie()
         self.default_wordlist_filename = get_complete_path('data/profanity_wordlist.txt')
+        self.default_urls_filename = get_complete_path('data/profane_sites.txt')
         self.load_profane_words()
+        self.load_profane_urls()
+
+    def load_profane_urls(self):
+        profane_urls = read_wordList(self.default_urls_filename)
+        for url in profane_urls:
+            self.censor_urls.add(url)
+
+    def censor_url(self, url):
+        if self.censor_urls.__contains__(url):
+            return '*'*len(url)
+        return url
 
     def censor(self, text, censor_char="*"):
 
@@ -49,38 +86,17 @@ class ProfanityFilter:
             self.fill_profane_wordset(profane_words)
 
     def fill_profane_wordset(self, profane_words):
-        self.generate_possible_profane_words(profane_words)
-
-    def generate_possible_profane_words(self, profane_words):
         for profane_word in profane_words:
-            self.dfs(profane_word, 0, [])
-
-    def dfs(self, profane_word, idx, char_list):
-        if idx == len(profane_word):
-            possible_profane_word = ''
-            for char in char_list:
-                possible_profane_word += char
-            if self.whiteList_trie.root is None or self.whiteList_trie.hasPrefix(possible_profane_word) is False:
-                self.profane_trie.insert(possible_profane_word)
-            return
-
-        if profane_word[idx] not in self.CHARS_MAPPING:
-            char_list.append(profane_word[idx])
-            self.dfs(profane_word, idx+1, char_list)
-            char_list.pop(len(char_list)-1)
-
-        else:
-            for char in self.CHARS_MAPPING[profane_word[idx]]:
-                char_list.append(char)
-                self.dfs(profane_word, idx + 1, char_list)
-                char_list.pop(len(char_list) - 1)
+            if self.whiteList_trie.hasPrefix(profane_word, 0, self.whiteList_trie.root):
+                continue
+            self.profane_trie.insert(profane_word)
 
     def censor_profane_words(self, message, censor_char):
         message = message.split()
         clean_message = ''
         for word in message:
             curr_word = ''
-            if self.profane_trie.hasPrefix(word):
+            if self.profane_trie.hasPrefix(word.lower(), 0, self.profane_trie.root):
                 for i in range(len(word)):
                     curr_word += censor_char
             else:
@@ -92,35 +108,11 @@ class ProfanityFilter:
         if self.profane_trie.hasPrefix(word):
             return True
         return False
-    
-    def get_image_analysis(self, URL):
-        r = requests.post(
-            "https://api.deepai.org/api/nsfw-detector",
-            data={
-                'image': URL,
-            },
-            headers={'api-key': '7b0ebb62-4127-46a7-877f-2d520f635a75'}
-        )
-        print(r.json()['output'])
-        
-    def get_image_profanity_score(self, image_url):
-        r = requests.post(
-            "https://api.deepai.org/api/nsfw-detector",
-            data={
-                'image': image_url,
-            },
-            headers={'api-key': '7b0ebb62-4127-46a7-877f-2d520f635a75'}
-        )
-        # if nsfw_score is more than 0.7 it is definitely profane
-        return r.json()['output']['nsfw-score']
 
-    def censor_image(self, image_url):
-        response = requests.get(image_url)
-        img = Image.open(BytesIO(response.content))
+    def add_profane_words(self, words):
+        for word in words:
+            self.profane_trie.insert(word)
 
-        # Applying GaussianBlur filter
-        gaussImage = img.filter(ImageFilter.GaussianBlur(10))
-        gaussImage.show()
-
-        # Save Gaussian Blur Image
-        gaussImage.save('images/image1.jpg')
+    def add_whitelist_words(self, words):
+        for word in words:
+            self.whiteList_trie.insert(word)
