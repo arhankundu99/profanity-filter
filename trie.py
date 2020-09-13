ad70199611fbe874efaa32e0f5a5b87471e9ae8a
Arhan Kundu <arhankundu99@gmail.com>
2020-09-13 15:39:18 +0530

    added map

--- trie.py
+++ trie.py
@@ -4,6 +4,18 @@ from collections import defaultdict
 class Trie:
     def __init__(self):
         self.root = defaultdict()
+        self.CHARS_MAPPING = {
+            "@": ("a", "o"),
+            "*": ("a", "i", "o", "u", "v", "e"),
+            "4": "a",
+            "6": "b",
+            "1": ("i", "l"),
+            "0": "o",
+            "3": ("e", "b"),
+            "$": "s",
+            "5": "s",
+            "7": "t"
+        }
 
     # @param {string} word
     # @return {void}
@@ -27,16 +39,18 @@ class Trie:
             return True
         return False
 
-    def hasPrefix(self, word):
-        current = self.root
-        for letter in word:
-            if letter not in current:
-                if "_end" in current:
-                    return True
-                return False
-            current = current[letter]
+    def hasPrefix(self, word, idx, current):
         if "_end" in current:
             return True
+        if idx == len(word):
+            return False
+        if word[idx] in self.CHARS_MAPPING:
+            for char in self.CHARS_MAPPING[word[idx]]:
+                if char in current:
+                    if self.hasPrefix(word, idx+1, current[char]):
+                        return True
+        elif word[idx] in current:
+            return self.hasPrefix(word, idx+1, current[word[idx]])
         return False
 
     # @param {string} prefix
@@ -44,6 +58,7 @@ class Trie:
     # Returns if there is any word in the trie
     # that starts with the given prefix.
     def startsWith(self, prefix):
+        prefix = prefix.lower()
         current = self.root
         for letter in prefix:
             if letter not in current:
